b7b4b5f75a0717db76e01d88386e484c7887e949
Abdalmohsen16 <100500608+Abdalmohsen16@users.noreply.github.com>
2022-03-21 00:26:54 +0300

    done

--- Exercise 1/ex1.js
+++ Exercise 1/ex1.js
@@ -1,5 +1,17 @@
-let planet;
+// let planet;
 
-(function travel(x = "زمردة") {
-  console.log("أود السفر إلى كوكب " + x);
-})(planet);
+// (function travel(x = "زمردة") {
+//   console.log("أود السفر إلى كوكب " + x);
+// })(planet);
+
+
+(function travel(x = 'زمردة') 
+{
+  console.log(
+    `اود السفر الى كوكب ${x}`
+  )
+}) ('المشترى')
+
+let planet = prompt('ادخل اسم الكوكب')
+
+travel(planet)
