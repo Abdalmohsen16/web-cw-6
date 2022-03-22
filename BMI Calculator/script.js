eae2f4a805434edecb1baba685fc0f30bf8870f9
Abdalmohsen16 <100500608+Abdalmohsen16@users.noreply.github.com>
2022-03-22 21:36:54 +0300

    done

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,1 +1,36 @@
 // هنا سوف تنشئ دالة calculate 
+
+function calculate() {
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+
+
+    let BMI = weight/(height*height);
+
+    let status = " "
+
+
+if (BMI < 18.5)
+{status = "تعاني من نقص في الوزن"}
+
+else if (BMI < 25)
+{status = "وزنك صحي"}
+ 
+else if (BMI < 30) 
+{status = "تعاني من زيادة في الوزن"} 
+
+else 
+{status = "تعاني من السمنة المفرطة"}
+
+
+document.getElementById("result").innerHTML = BMI;
+document.getElementsByClassName("comment")[0].innerHTML = status
+
+let x = document.getElementById('weight').value;
+let y = document.getElementById('height').value;
+
+if (x == "" || y == "") {
+    alert("يرجى ادخال جميع البيانات المطلوبه");
+    return false;
+  }
+}
